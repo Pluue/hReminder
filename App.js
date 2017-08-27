f77951bfc8f19d1e7bfc56fd7fa735cae0f25a71
Sam Hoch <Sam Hoch>
2017-08-28 09:19:11 +1000

    first draft, needs styling and deletion

--- App.js
+++ App.js
@@ -11,54 +11,38 @@ import {
   TouchableHighlight,
 } from 'react-native';
 import { Task, taskStyle } from './Task.js';
-import { TaskForm, formStyle } from './TaskForm.js';
+import { TaskForm } from './TaskForm.js';
 
 // cd C:\Users\Sam\Desktop\Projects\hReminder
-var i = 1;
+var taskKey = 1;
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      newTaskTitle: '',
       tasks: [],
     };
   }
+  //Adds a new task to the tasks array in state, method of communcation between TaskForm and App
+  onAddNewTask = task => {
+    this.setState({ tasks: [...this.state.tasks, task] });
+  };
   render() {
     return (
       <View>
         <View style={styles.header} padding={40}>
           <Text style={styles.test}> hReminder </Text>
         </View>
-        <TaskForm />
-        {/* <View>
-          <TextInput
-            value={this.state.newTaskTitle}
-            onChangeText={text => this.setState({ newTaskTitle: text })}
-          />
-          <Button
-            title={'Title'}
-            onPress={() => {
-              this.setState({
-                tasks: [...this.state.tasks, this.state.newTaskTitle],
-                newTaskTitle: '',
-              });
-              Keyboard.dismiss();
-            }}
-          >
-            Add Task
-          </Button>
-        </View> */}
+        {/*instance of new task form, passing the onAddNewTask function from the parent*/}
+
+        <TaskForm onAddNewTask={this.onAddNewTask} />
+
+        {/*ScrollView allows for infinite scrolling as more components are added*/}
         <ScrollView>
-          <View
-            style={{ flexDirection: 'column', height: '100%', padding: 20 }}
-          >
-            {taskList.map(task =>
-              <Task
-                title={task.label}
-                subtitle={task.subtitle}
-                description={task.description}
-                key={(i = i + 1)}
-              />
+          <View style={{ flexDirection: 'column', height: '90%', padding: 20 }}>
+            {sortTasksDefault(this.state.tasks)}
+            {/*Creates a new Task component for each item in tasks, passing through the task as props*/}
+            {this.state.tasks.map(task =>
+              <Task newTask={task} key={(taskKey = taskKey + 1)} />
             )}
           </View>
         </ScrollView>
@@ -66,8 +50,37 @@ export default class App extends React.Component {
     );
   }
 }
+function sortTasksDefault(tasks) {
+  let length = tasks.length;
+  for (let i = 0; i < length - 1; i++) {
+    let max = i;
+    for (let j = i + 1; j < length; j++) {
+      if (
+        Task.getDefaultSortIndex(tasks[j]) >
+        Task.getDefaultSortIndex(tasks[max])
+      ) {
+        max = j;
+      }
+    }
+    if (max != i) {
+      let tmp = tasks[i];
+      tasks[i] = tasks[max];
+      tasks[max] = tmp;
+    }
+  }
+  console.log(tasks);
+}
 
-export var taskList = [];
+subjectList = [
+  {
+    name: 'Methods',
+    col: 'blue',
+  },
+  {
+    name: 'SoftwareDev',
+    col: 'red',
+  },
+];
 
 const styles = StyleSheet.create({
   container: {
--- TaskForm.js
+++ TaskForm.js
@@ -5,33 +5,46 @@ import {
   View,
   Text,
   Modal,
-  TouchableHighlight,
   Button,
+  Picker,
+  Slider,
+  DatePickerAndroid,
+  Alert
 } from 'react-native';
-import { taskList } from './App.js';
+var marg = 5
 
-export default class TaskForm extends React.Component {
+export class TaskForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isShown: false,
+      canSubmit: [false, false],
       newTask: {
         label: '',
         subtitle: '',
         description: '',
         dueDate: '',
         reminderDate: '',
-        subject: '',
-        priority: '',
+        subject: {},
+        priority: 1,
+        //daysFromNow: this.getDaysFromNow(), //WILL DEFINITELY CAUSE PROBLEMS
+        //defaultSortIndex: Math.pow(1.5, this.state.newTask.priority) / this.state.newTask.daysFromNow
       },
     };
+  }
+  getDaysFromNow() {
+    return Math.round(
+      Math.abs(
+        (new Date().getTime() - this.state.newTask.dueDate.getTime()) / (24 * 60 * 60 * 1000)
+      ) // hours*minutes*seconds*milliseconds = full unix day
+    );
   }
   showForm(visible) {
     this.setState({ isShown: visible });
   }
   render() {
     return (
-      <View backgroundColor={'green'}>
+      <View>
         <Modal
           animationType={'fade'}
           transparent={true}
@@ -40,22 +53,38 @@ export default class TaskForm extends React.Component {
             this.setState({ isShown: false });
           }}
         >
+        {/*this view is the shadowed window behind the popup*/}
           <View
             backgroundColor={'rgba(0, 0, 0, 0.4)'}
             height={'100%'}
-            justifyContent="center"
+            justifyContent="flex-start"
             alignItems="center"
+            paddingVertical={30}
           >
+          {/*the popup itself, this view disctates the size and color*/}
             <View
               backgroundColor={'white'}
-              height={'75%'}
+              height={350}
               width={'75%'}
-              justifyContent="center"
-              alignItems="flex-start"
+              
             >
+            {/*this view dictates the alignment of the title*/}
+               <View
+               fontSize={60}
+                height={60}
+                justifyContent="flex-start"
+                alignItems="center"
+                >
+                <Text>New Task</Text>
+              </View> 
+              {/*this view dictates the alignment of the inputs*/}
+              <View
+                justifyContent="center"
+                alignItems="flex-start">
+
               <TextInput
                 style={formStyles.textInput}
-                palceholder={'Title'}
+                placeholder={'Title'}
                 value={this.state.newTask.label}
                 onChangeText={text =>
                   this.setState({
@@ -81,19 +110,85 @@ export default class TaskForm extends React.Component {
                   })}
               />
 
-              <Button
+              <Slider
+                width="50%"
+                step={1}
+                maximumValue={10}
+                minimumValue={0}
+                marginVertical = {5}
+                onValueChange={priority =>
+                  this.setState({
+                    newTask: { ...this.state.newTask, priority: priority },
+                  })}
+              /> 
+              
+              <Button title="Due Date" onPress={async () => {try {
+                var {action, year, month, day} = await DatePickerAndroid.open({
+                  minDate: new Date(),
+                  date: new Date()
+                });
+                if (action !== DatePickerAndroid.dismissedAction) {
+                  this.setState({canSubmit: [true, this.state.canSubmit[1]], newTask: { ...this.state.newTask, dueDate: new Date(year, month, day) }})
+                
+                }
+              } catch ({code, message}) {
+                console.warn('Cannot open date picker', message);
+              }}}></Button>
+
+              <View width={'50%'}>
+                <Picker
+                marginVertical = {5}
+                  mode="dropdown"
+                  alignItems={'center'}
+                  selectedValue={"Select"}
+                  onValueChange={(itemValue, itemIndex) =>{
+                    if(itemValue){
+                      this.setState({
+                      newTask: { ...this.state.newTask, subject: itemValue }, canSubmit: [this.state.canSubmit[0], true]
+                    })}
+                  
+                  }
+                    
+                  
+                    }
+                >
+                {/* {this.props.subjectList.map( subject => <Picker.Item label={subject.name} value={subject} key={j=(j+1)}/>)} */}
+                <Picker.Item label="-Subject-" value={false} />
+                <Picker.Item label="Methods" value={{name: "Methods", col: "red"}} />
+                <Picker.Item label="Software Dev" value={{name: "Software Dev", col: "blue"}} />
+                <Picker.Item label="Literature" value={{name: "Literature", col: "green"}} />
+                <Picker.Item label="English" value={{name: "English", col: "yellow"}} />
+                <Picker.Item label="Misc" value={{name: "Misc", col: "#efefef"}} />
+                
+                </Picker>
+              </View>
+              <Button 
+              
+                justifyContent="flex-start"
                 title="Submission Button"
                 onPress={() => {
-                  taskList.push(this.state.newTask);
+                  if(this.state.canSubmit[0] && this.state.canSubmit[1]){
+                  this.props.onAddNewTask(this.state.newTask);
                   this.showForm(!this.state.isShown);
+                  this.setState({
+                    canSubmit: [false, false], newTask: { label: '', subtitle: '', description: '', subject: {}, defaultSortIndex: 1, daysFromNow: ''},
+                  });
+                }else{Alert.alert('Invalid Data', 'Please add a valid due date and subject', [{text: 'OK'}])}
                 }}
               />
+            </View>
+            {/* <View 
+              justifyContent="center"
+              alignItems="flex-end">
+              <Text>Right hand side</Text>
+            </View> */}
+
             </View>
           </View>
         </Modal>
 
         <Button
-          title="New Task Button"
+          title="New Task"
           onPress={() => {
             this.showForm(true);
           }}
@@ -103,11 +198,13 @@ export default class TaskForm extends React.Component {
   }
 }
 
-export const formStyles = StyleSheet.create({
+
+const formStyles = StyleSheet.create({
   textInput: {
     width: '40%',
     borderColor: '#000',
-    paddingHorizontal: 1,
+    marginVertical: 5,
+    paddingHorizontal: 3,
     paddingVertical: 2,
   },
 });
